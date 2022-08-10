e37bd7ed4ce5262eb7de3689f5b11f93b1d5f7b8
Toshio Mori <tmori@ToshionoMacBook-Air.local>
2022-08-10 13:51:05 +0900

    format

--- TN_Julia/d-mps.jl
+++ TN_Julia/d-mps.jl
@@ -2,20 +2,31 @@ include("./MPS.jl")
 using .MPSforQuantum
 using LinearAlgebra
 
-#N = 20
-N = 10
-#C0 = normalize!(rand(ComplexF64, 2^N)) # for random state
-C0 = zeros(ComplexF64, 2^N)
-C0[1] = 1 # '|000>'
 
-# #D = 10
-# eps = 1e-3
-# mps = MPS(C0, eps)
-# println("MPS's information")
-# mps_size(mps)
-# Hadamard = convert(Array{ComplexF64,2}, [1 1; 1 -1] ./ sqrt(2))
-# mps = OneQubitGate(mps, Hadamard, 0) # -> '001' and '000'
-# println(mps)
+# function SVD2(C::Array{ComplexF64,2}, eps::Float64)
+#     #eps = 1e-2
+#     A = svd(C)
+#     filter!((x) -> x > eps, A.S)
+#     l = length(A.S)
+#     return A.U[:, 1:l], diagm(A.S), A.Vt[1:l, :]
+# end
+
+function SVD2(C::Array{ComplexF64,2}, eps::Float64)
+    #eps = 1e-2
+    A = svd(C)
+    filter!((x) -> x > eps, A.S)
+    l = length(A.S)
+    return A.U[:, 1:l], diagm(A.S) * A.Vt[1:l, :]
+end
+
+function SVD2(C::Array{ComplexF64,2}, D::Int64)
+    A = svd(C)
+    if length(A.S) > D
+        return A.U[:, 1:D], diagm(A.S[1:D]) * A.Vt[1:D, :]
+    else
+        return A.U[:, :], diagm(A.S) * A.Vt[:, :]
+    end
+end
 
 function MPS2(C::Array{ComplexF64,1}, param)
     # C = copy(C0)
@@ -28,7 +39,7 @@ function MPS2(C::Array{ComplexF64,1}, param)
     r = 1
     for i = 1:N-1
         C = reshape(C, d * r, d^(N - i))
-        tmp_A, C = SVD(C, param)
+        tmp_A, C = SVD2(C, param)
         r = size(tmp_A, 2)
         col = convert(Int64, size(tmp_A, 1) / 2)
         push!(arrs, [tmp_A[1:col, :], tmp_A[(col+1):col*2, :]])
@@ -40,6 +51,20 @@ function MPS2(C::Array{ComplexF64,1}, param)
     return arrs
 end
 
+#N = 20
+N = 10
+#C0 = normalize!(rand(ComplexF64, 2^N)) # for random state
+C0 = zeros(ComplexF64, 2^N)
+C0[1] = 1 # '|000>'
+
 eps = 1e-3
-mps = MPS2(C0, eps)
+mps = MPS(C0, eps)
 println("MPS's information")
+mps_size(mps)
+Hadamard = convert(Array{ComplexF64,2}, [1 1; 1 -1] ./ sqrt(2))
+mps = OneQubitGate(mps, Hadamard, 0) # -> '001' and '000'
+println(mps)
+
+# eps = 1e-3
+# mps = MPS2(C0, eps)
+# println("MPS's information")
